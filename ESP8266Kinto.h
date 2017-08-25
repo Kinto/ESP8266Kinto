5a4ca7e79e8150500086d0d83803812fa4b9e769
Julien Lebunetel <julien@lebunetel.com>
2017-08-25 12:20:45 +0200

    argument name correction

--- ESP8266Kinto.h
+++ ESP8266Kinto.h
@@ -19,7 +19,7 @@ class Kinto
     Kinto(char* server, char* token, char* secret, char* bucket, char* collection, char* fingerprint);
     void post(String json);
     void patch(String id, String json);
-    String getRecord(String json);
+    String getRecord(String id);
 
   // library-accessible "private" interface
   private:
